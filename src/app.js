a37332778c9d8a6fd9978e6e2e7c04a0faf1ad3e
Nataly-04 <pau_nata14@hotmail.com>
2026-05-15 10:22:38 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronoun = ['the', 'our', 'my'];
+  let adj = ['small', 'old', 'beautiful'];
+  let noun = ['cat', 'house', 'bike'];
+  let domain = ['.es', '.com', '.io'];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let d of domain) {
+          console.log(p + a + n + d)
+        }
+
+
+      }
+    }
+  }
+
 };
